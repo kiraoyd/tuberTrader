3728ad331e2f383922e8900ac09a6fdaf5787f0c
Kira Klingenberg <kirak@pdx.edu>
2023-03-08 00:01:18 -0800

    Started writing code to call backendroute to retrieve searched profile. Still to do: Add this to the App, weave in stuff we learned today about wrapping state at the top of the tree

--- frontend/src/components/Search.tsx
+++ frontend/src/components/Search.tsx
@@ -1,19 +1,33 @@
 
-
+import {useEffect, useState} from "react";
+//https://www.emgoto.com/react-search-bar/
+// https://beta.reactjs.org/reference/react-dom/components/input
+//import axios
+import axios from "axios";
 //create search bar component
 
-export const SearchBar = () => (
-    <form action = "/" method = "get">
+export const SearchBar = () => {
+    //TODO I can save the island name from the search bar, but how to get it to the "search results page"
+    let [island, setIslandName] = useState("")
+
+    function getIslandName(e:any){
+        e.preventDefault(); //prevent browser from reloading page
+        setIslandName(e.target.value) //island = e.target.value
+    }
+
+    return(
+    <form action = "/" method = "get" onSubmit={getIslandName}>
         <label htmlFor = "header-search">
             <span className="visually-hidden"> Find an Island! </span>
         </label>
         <input
             type="text"
+            value={island}
             id="island-search"
             placeholder="Search islands"
             name="s"
             />
         <button type="submit"> Search </button>
     </form>
-);
+)};
 
