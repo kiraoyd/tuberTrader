bc2c5997e8ccc03fc274cecd9fe64fea73acd5aa
Kira Klingenberg <kirak@pdx.edu>
2023-05-08 21:03:52 -0700

    Messed up config files. Swapped entire backend out for current version of FullStack's doggr repo. Combed the config files to make sure they catch my auth stuff (things the class isn't on yet), and am about to start moving over my database models and all the routes to this new fresh backend template.

--- backend/src/types.ts
+++ backend/src/types.ts
@@ -1,114 +1,18 @@
-/** @module Types */
-import cors from "cors";
-import {FastifyInstance, FastifyReply, FastifyRequest, RouteShorthandOptions} from "fastify";
-import {User} from "./db/models/user";
-import {IPHistory} from "./db/models/ip_history";
-import {Profile} from "./db/models/profile";
-import {Transactions} from "./db/models/transactions";
-import {SellingPriceHistory} from "./db/models/sellingPriceHistory";
-import {sellingPriceHistory1678485063600} from "./db/migrations/1678485063600-sellingPriceHistory";
-
-/**
- * Interfacing for post/users body
- */
-export interface IPostUsersBody {
-    name: string,
-    email: string,
+export type ICreateUsersBody = {
+	name: string,
+	email: string,
+	password: string,
+	petType: string
 }
 
-/**
- * Response type for post/users
- */
-export type IPostUsersResponse = {
-    /**
-     * User created by request
-     */
-    user: User,
-    /**
-     * IP Address user used to create account
-     */
-    ip_address: string
+export type IUpdateUsersBody = {
+	name: string,
+	id: number,
+	petType: string
 }
 
-/**
- * Params type for get/user/:username
- */
-export type IGetUserParams ={
-    username: string;
-
-}
-
-
-/**
- * Interfacing for post/profiles
- */
-
-export interface IPostProfilesBody {
-    islandName: string,
-    picture: string,
-    turnipsHeld: number,
-    pricePaid: number,
-    ownerId: number
-}
-
-/**
- * Response type for post/profiles
- */
-export type IPostProfilesResponse = {
-    profile: Profile
-}
-
-
-/**
- * Params type for get/profile/:islandName
- */
-export type IGetProfileParams = {
-    islandName: string;
-}
-
-/**
- * interface for post/transactions
- */
-export interface IPostTransactionsBody {
-    numberSold: number,
-    priceSold: number,
-    profits: number,
-    seller: User,
-    host: Profile
-}
-
-export type IPostTransactionsResponse = {
-    transaction: Transactions
-}
-
-/**
- * querystring type for get/transaction
- */
-export type IQuerystring = {
-    sellerID: number;
-    islandID: number;
-}
-
-export enum amPM {
-    am = "am",
-    pm ="pm",
-    AM ="AM",
-    PM ="PM"
-}
-
-/**
- * Interface for posting a new selling price
- */
-export interface IPostPriceBody{
-    island: number,
-    price: number,
-    //timeOfDay: amPMpnp
-    timeOfDay: string,
-    currentDate: string
-}
-
-
-export type IPostPriceResponse = {
-    price: SellingPriceHistory
-    error: string,
+export type ICreateMessage = {
+	sender_id: number,
+	receiver_id: number,
+	message: string,
 }
